dc039b78e412c41bc5e9a2ba3ae3d9741ad6250b
elotwo <obiagwu94@gmail.com>
2024-10-11 13:10:05 +0100

    typescript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -25,6 +25,9 @@ const director1: Director = {
 	contract: true,
 	numberOfReports: 14,
 };
+function printTeacher(firstName: string, lastName: string): string {
+	return `${firstName[0]}.${lastName}`;
+};
 // should print
 // // Object
 // // contract: false
